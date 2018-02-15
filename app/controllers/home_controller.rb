a1659851ebd5138de35556659af246741ad55a44
FabienMllts <fabienmallet@gmail.com>
2018-02-15 22:21:11 +0100

    lancement heroku

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,27 @@
+class HomeController < ApplicationController
+
+	def index
+
+		@price=nil
+		@currency=nil
+
+		if params[:search]
+			@currency = params_choice[:choice]
+		end
+
+		@currencies = CryptoPrice.new.cryptocurrencies 
+		
+		@price = CryptoPrice.new.perform(@currency)
+	end
+
+	private
+
+	def params_choice
+		params.require(:search).permit(:choice)
+	end
+end
+
+
+
+
+
